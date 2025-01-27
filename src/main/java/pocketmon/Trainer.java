8f3def09db169585b73db437b3cbbf412dd55643
Labise99 <teenyrabbit@gmail.com>
2025-01-27 16:44:42 +0900

    진화 기능 수정, 테스트 전

--- src/main/java/pocketmon/Trainer.java
+++ src/main/java/pocketmon/Trainer.java
@@ -58,11 +58,6 @@ public class Trainer implements ITrainer {
     public Pokemon encounterWildPokemon() {
         List<Pokemon> wildPokemons = List.of(
                 new FlyPokemon("구구", 30, 3), // 구구도 FlyPokemon으로 처리
-                new FlyPokemon("피죤", 70, 10),
-                new SurfPokemon("잉어킹", 25, 2),
-                new LegendPokemon("루기아", 120, 20),
-                new MysticPokemon("뮤츠", 150, 30),
-                new SurfPokemon("꼬부기", 20,3),
                 new LunaPokemon("푸린", 30, 20),
                 new LunaPokemon("삐삐", 40, 20));
         Random random = new Random();
